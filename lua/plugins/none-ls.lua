428479ccc1e3e7d3c76e82ce145cf05efbb84ac8
Brandon Corey <brandon@legalfit.com>
2024-11-06 17:07:15 -0600

    add eslint, prettier, and ruff

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,15 +1,22 @@
 return {
-    {
-        "nvimtools/none-ls.nvim",
-        config = function()
-            local null_ls = require("null-ls")
-            null_ls.setup({
-                sources = {
-                    null_ls.builtins.formatting.stylua,
-                },
-            })
+	{
+		"nvimtools/none-ls.nvim",
+		dependencies = {
+			"nvimtools/none-ls-extras.nvim",
+		},
+		config = function()
+			local null_ls = require("null-ls")
+			null_ls.setup({
+				sources = {
+					null_ls.builtins.formatting.stylua,
+					require("none-ls.diagnostics.eslint_d"),
+					null_ls.builtins.formatting.prettier,
+					require("none-ls.diagnostics.ruff"),
+					require("none-ls.formatting.ruff"),
+				},
+			})
 
-            vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-        end,
-    },
+			vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+		end,
+	},
 }
